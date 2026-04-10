ae47f6c3fa948fced14684817ff549972c99737c
Leonardo Sivori <leonardo.sivori@studenti.unipd.it>
2026-04-11 00:12:49 +0200

    Game Screen Layout now responds correctly to change of orientation Made better use of Composables Added all 6 colors

--- app/src/main/java/it/unipd/dei/sivorleon/simon/Game.kt
+++ app/src/main/java/it/unipd/dei/sivorleon/simon/Game.kt
@@ -1,14 +1,14 @@
 package it.unipd.dei.sivorleon.simon
 
+import android.content.res.Configuration
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
@@ -20,12 +20,20 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.unit.dp
+import it.unipd.dei.sivorleon.simon.ui.theme.Blue
+import it.unipd.dei.sivorleon.simon.ui.theme.Cyan
 import it.unipd.dei.sivorleon.simon.ui.theme.Green
+import it.unipd.dei.sivorleon.simon.ui.theme.Magenta
 import it.unipd.dei.sivorleon.simon.ui.theme.Red
+import it.unipd.dei.sivorleon.simon.ui.theme.Yellow
 
 @Composable
 fun Game(onEndGame: (String) -> Unit) {
+    val orientation = LocalConfiguration.current.orientation
+
     var currGame by rememberSaveable { mutableStateOf("") }
 
     fun addToGame(code: String) {
@@ -36,53 +44,91 @@ fun Game(onEndGame: (String) -> Unit) {
         }
     }
 
-    Column() {
-        Column() {
+    @Composable
+    fun ColorElement(color: Color, code: String) {
+        Box(
+            modifier = Modifier
+                .size(width = 100.dp, height = 100.dp)
+                .background(color)
+                .clickable { addToGame(code) }
+        )
+    }
+
+    @Composable
+    fun ColorGrid(modCol: Modifier) {
+        Column(
+            modifier = modCol,
+            verticalArrangement = Arrangement.SpaceEvenly
+        ) {
             Row(
                 modifier = Modifier.fillMaxWidth(),
                 horizontalArrangement = Arrangement.SpaceEvenly
             ) {
-                Box(
-                    modifier = Modifier
-                        .size(width = 100.dp, height = 100.dp)
-                        .background(Red)
-                        .clickable { addToGame("R") }
-                )
-                Box(
-                    modifier = Modifier
-                        .size(width = 100.dp, height = 100.dp)
-                        .background(Green)
-                        .clickable { addToGame("G") }
-                )
+                ColorElement(Red, "R")
+                ColorElement(Green, "G")
             }
-
-            Spacer(modifier = Modifier.height(16.dp))
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceEvenly
+            ) {
+                ColorElement(Blue, "B")
+                ColorElement(Magenta, "M")
+            }
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceEvenly
+            ) {
+                ColorElement(Yellow, "Y")
+                ColorElement(Cyan, "C")
+            }
         }
+    }
 
-        Spacer(modifier = Modifier.height(32.dp))
-
-        TextField(
-            value = currGame,
-            onValueChange = {},
-            enabled = false,
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(32.dp, 0.dp)
-        )
-
-        Spacer(modifier = Modifier.height(32.dp))
-
-        Row(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.SpaceEvenly
+    @Composable
+    fun TextButtons() {
+        Column(
+            modifier = Modifier.fillMaxHeight(),
+            verticalArrangement = Arrangement.SpaceEvenly
         ) {
-            Button(onClick = { currGame = "" }) {
-                Text(text = "Cancella")
-            }
+            TextField(
+                value = currGame,
+                onValueChange = {},
+                enabled = false,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(48.dp, 16.dp)
+            )
 
-            Button(onClick = { onEndGame(currGame); currGame = "" }) {
-                Text(text = "Fine Partita")
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceEvenly
+            ) {
+                Button(
+                    onClick = { currGame = ""}
+                ) {
+                    Text(text = "Cancella")
+                }
+
+                Button(
+                    onClick = { onEndGame(currGame); currGame = "" }
+                ) {
+                    Text(text = "Fine Partita")
+                }
             }
+        }
+    }
+
+    if (orientation == Configuration.ORIENTATION_PORTRAIT) {
+        Column() {
+            ColorGrid(Modifier.fillMaxWidth().fillMaxHeight(0.5f).padding(top = 16.dp))
+
+            TextButtons()
+        }
+    } else {
+        Row() {
+            ColorGrid(Modifier.fillMaxWidth(0.5f).fillMaxHeight())
+
+            TextButtons()
         }
     }
 }
